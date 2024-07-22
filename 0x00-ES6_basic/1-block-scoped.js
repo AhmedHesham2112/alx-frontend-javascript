fed36413044099ecf7a87c870b9cf352bc92f56f
AhmedHesham2112 <AhmedHesham2112>
2024-07-22 13:04:28 +0300

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -5,6 +5,9 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     const task = false;
     const task2 = true;
+    if (task && task2) {
+      return [task, task2];
+    }
   }
 
   return [task, task2];
