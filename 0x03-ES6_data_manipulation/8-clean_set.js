1c0f4b077ff6fda9eb246659c30c6f1d12360af2
AhmedHesham2112 <AhmedHesham2112>
2024-07-29 23:06:01 +0300

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,6 @@
 export default function cleanSet(set, startString) {
-  let arr = [];
-  if (
-    typeof set !== 'object' ||
-    typeof startString !== 'string' ||
-    startString.length === 0
-  ) {
+  const arr = [];
+  if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
   for (const word of set) {
@@ -12,5 +8,5 @@ export default function cleanSet(set, startString) {
       arr.push(word.slice(startString.length));
     }
   }
-  return arr.join('-')
+  return arr.join('-');
 }
